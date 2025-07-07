922c33f0da8b943c0f5f12cc3c827dd2195d9bcb
Edward Qiu <edward@growthylab.ai>
2025-07-07 17:40:27 +0800

    fix: style

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -6,15 +6,15 @@ import Image from "next/image";
 export const Hero = () => {
   return (
     <section className="md:max-w-custom mx-4 xl:mx-auto py-16 lg:py-25">
-      <div className="flex flex-col xl:flex-row xl:gap-12 items-center">
+      <div className="flex flex-col xl:flex-row items-center">
         {/* Left Content */}
-        <div className="flex-1 flex flex-col justify-between xl:min-h-[585px]">
+        <div className="flex-2 flex flex-col justify-between xl:min-h-[585px]">
           <HeroTitle />
           <TrustedBy />
         </div>
 
         {/* Right Content - Feature Cards */}
-        <div className="space-y-[30px] py-4 flex flex-col items-start md:items-center xl:items-end w-full">
+        <div className="flex-1 space-y-[30px] py-4 flex flex-col items-start md:items-center xl:items-end w-full">
           <FeatureCard
             src="/homepage/feat1.svg"
             alt="Built for Connection"
