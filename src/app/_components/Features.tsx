65aa73dbba22dfb4a4f6ee7600bbdf30d3f7d076
Edward Qiu <edward@growthylab.ai>
2025-07-07 17:12:02 +0800

    style：responsivible

--- src/app/_components/Features.tsx
+++ src/app/_components/Features.tsx
@@ -2,42 +2,45 @@ import Image from "next/image";
 
 export const Features = () => {
   return (
-    <section className="mx-auto px-4 sm:px-6 py-24 bg-[#FAFAFB] xl:px-[120px]">
-      <div className="text-left mb-16 max-w-5xl font-outfit">
-        <h2 className="text-4xl sm:text-5xl lg:text-6xl font-semibold text-[#111111] mb-6 maintitle">
-          Smarter <span className="text-blue-500">Advertising</span>, Made
-          Simple.
-        </h2>
-        <p className="subtitle">
-          AI that understands your goals. Plan, launch, and scale Meta campaigns
-          effortlessly — with a partner that learns, adapts, and grows with you.
-        </p>
-      </div>
+    <section className="w-full px-4 py-24 bg-[#FAFAFB]">
+      <div className="max-w-custom xl:mx-auto flex flex-col justify-center items-start">
+        <div className="text-left mb-16 max-w-5xl font-outfit">
+          <h2 className="text-4xl sm:text-5xl lg:text-6xl font-semibold text-[#111111] mb-6 maintitle">
+            Smarter <span className="text-blue-500">Advertising</span>, Made
+            Simple.
+          </h2>
+          <p className="subtitle">
+            AI that understands your goals. Plan, launch, and scale Meta
+            campaigns effortlessly — with a partner that learns, adapts, and
+            grows with you.
+          </p>
+        </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8 xl:gap-[75px] justify-items-center">
-        <FeatureCard
-          title="Lightning-Fast Launch"
-          imageSrc="/homepage/func1.png"
-          imageAlt="Feature 1"
-          imageWidth={290}
-          imageHeight={234}
-        />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8 xl:gap-[75px] w-full justify-items-center">
+          <FeatureCard
+            title="Lightning-Fast Launch"
+            imageSrc="/homepage/func1.png"
+            imageAlt="Feature 1"
+            imageWidth={290}
+            imageHeight={234}
+          />
 
-        <FeatureCard
-          title="Always-On AI Optimization"
-          imageSrc="/homepage/func2.png"
-          imageAlt="Feature 2"
-          imageWidth={290}
-          imageHeight={234}
-        />
+          <FeatureCard
+            title="Always-On AI Optimization"
+            imageSrc="/homepage/func2.png"
+            imageAlt="Feature 2"
+            imageWidth={290}
+            imageHeight={234}
+          />
 
-        <FeatureCard
-          title="Self-improving Ad Engine"
-          imageSrc="/homepage/func3.png"
-          imageAlt="Feature 3"
-          imageWidth={290}
-          imageHeight={234}
-        />
+          <FeatureCard
+            title="Self-improving Ad Engine"
+            imageSrc="/homepage/func3.png"
+            imageAlt="Feature 3"
+            imageWidth={290}
+            imageHeight={234}
+          />
+        </div>
       </div>
     </section>
   );
